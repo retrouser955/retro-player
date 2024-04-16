b8b52b8aaf3c654d8695b1d15acb357c2c572025
Retro <htoomyataung382@gmail.com>
2024-04-16 20:25:30 +0630

    Improve start up script

--- startapp.js
+++ startapp.js
@@ -1,14 +1,58 @@
 const { exec, execSync } = require("node:child_process");
 
 (async () => {
+    function installDistutils() {
+        return new Promise((resolve, reject) => {
+            console.log("Executing python -m pip install packaging")
+
+            const pro = exec("python -m pip install packaging")
+    
+            pro.stdout.on("data", (d) => {
+                process.stdout.write(d)
+            })
+    
+            pro.on("exit", (code) => {
+                if(code !== 0) {
+                    reject("Installed failed")
+                }
+    
+                resolve()
+            })
+        })
+    }
+
+    let packageManager = "yarn"
+
+    try {
+        execSync("yarn --version")
+    } catch {
+        packageManager = "npm install"
+    }
+
+    try {
+        const result = execSync("python --version").toString()
+
+        const justVersion = result.replace("Python ", "")
+
+        if(!justVersion.startsWith("3.10")) console.warn("[ WARNING ] This project was built was python 3.10. Higher or lower versions may not work")
+
+        if(justVersion.split(".")[1] > 11) {
+            console.warn("[ WARNING ] Setting up distutils ...")
+
+            await installDistutils()
+        }
+    } catch {
+        throw new Error("Python not detected!")
+    }
+
     function installSite() {
         return new Promise((resolve, reject) => {
-            console.log("Started installing dependencies for the site ... [ CMD: yarn ]")
+            console.log(`Started installing dependencies for the site ... [ CMD: ${packageManager} ]`)
 
-            const installSite = exec(`cd "./retro-player-site" && yarn`)
+            const installSite = exec(`cd "./retro-player-site" && ${packageManager}`)
     
             installSite.stdout.on("data", (d) => {
-                console.log(`[ SITE INSTALLATION LOG ] ${d}`)
+                process.stdout.write(`${d}`)
             })
     
             installSite.on("exit", (code) => {
@@ -23,12 +67,12 @@ const { exec, execSync } = require("node:child_process");
 
     function installExe() {
         return new Promise((resolve, reject) => {
-            console.log("Started installing dependencies for the app ... [ CMD: yarn ]")
+            console.log(`Started installing dependencies for the app ... [ CMD: ${packageManager} ]`)
 
-            const installExe = exec(`cd "./retro-player-exe" && yarn`)
+            const installExe = exec(`cd "./retro-player-exe" && ${packageManager}`)
     
             installExe.stdout.on("data", (d) => {
-                console.log(`[ APP INSTALLATION LOG ] ${d}`)
+                process.stdout.write(`${d}`)
             })
     
             installExe.on("exit", (code) => {
